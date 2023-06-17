eb3f56b578af4cce5ec9d9de656cad1b28c7d52a
Hemant <hemant@gmail.com>
2023-06-17 22:53:56 +0530

    fix: add new lone

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello In The World Of Hyderaabad  Biryaani!");
+        System.out.println("I am New in this world");
+    }
+}
